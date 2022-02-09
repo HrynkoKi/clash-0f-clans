a12da0f30e5578532a8420438613d408aac1577c
Kir <ki.hrynko@gmail.com>
2022-02-09 13:27:32 +0300

    first commit

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template v-slot:header>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      <h2>Use on the project</h2>
+      <ul>
+        <li>Vue3</li>
+        <li>Router</li>
+        <li>Vuex</li>
+        <li>Scss</li>
+      </ul>
+    </div>
+  </div>
+</template>
